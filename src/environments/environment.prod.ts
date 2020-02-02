00b619928db7e0fb659702bc22a08badf16b2985
shubham <shubhampawar355@gmail.com>
2020-02-03 04:04:11 +0530

    third

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
 
-  url: "http://localhost:8080/project1.1/"
+  //url: "http://localhost:8080/project1.1/"
 
 
-  //url: "http://13.233.231.83:9090/project1.1/"
+  url: "http://13.233.197.95:9090/project1.1/"
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,9 +5,9 @@
 export const environment = {
   production: false,
 
-  url: "http://localhost:8080/project1.1/"
+//  url: "http://localhost:8080/project1.1/"
 
-//  url: "http://13.233.231.83:9090/project1.1/"
+  url: "http://13.233.197.95:9090/project1.1/"
 
 };
 
