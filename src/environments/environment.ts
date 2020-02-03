98de417a2cfbbf59a8dc8bdf0fdaf393c7d026ef
shubham <shubhampawar355@gmail.com>
2020-02-03 10:07:10 +0530

    third

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
 
 //  url: "http://localhost:8080/project1.1/"
 
-  url: "http://13.233.197.95:9090/project1.1/"
+  url: "http://13.234.197.117:9090/project1.1/"
 
 };
 
